cdc50af53ddbe04d338d11823a280425af616505
NordineFR <farisseaz@outlook.fr>
2023-08-16 22:21:40 +0100

    added main section and dynamic bg

---
+++ components/GeneralWeatherCard.tsx
@@ -0,0 +1,47 @@
+import { useState, useEffect } from 'react';
+
+type Styles = {
+  backgroundColor: string;
+  color: string;
+};
+
+const GeneralWeather = () => {
+  const [styles, setStyles] = useState<Styles>({
+    backgroundColor: '',
+    color: ''
+  });
+
+  const weatherStatus = 'cloudy';
+
+  useEffect(() => {
+    const weatherStyles = getWeatherStyles(weatherStatus);
+    setStyles(weatherStyles);
+  }, []);
+
+  const getWeatherStyles = (weatherStatus: string): Styles => {
+    if (weatherStatus === 'sunny') {
+      return {
+        backgroundColor: '#9FC7F6', // Hexcode for blue color
+        color: 'white' // Set the text color for sunny
+      };
+    } else if (weatherStatus === 'cloudy') {
+      return {
+        backgroundColor: '#0F1621', // Hexcode for dark gray color
+        color: 'white' // Set the text color for cloudy
+      };
+    }
+    // Add more conditions for other weather statuses and styles
+    return {
+      backgroundColor: '',
+      color: ''
+    };
+  };
+
+  return (
+    <div className="m-6 p-6 rounded-lg" style={styles}>
+      GeneralWeather
+    </div>
+  );
+}
+
+export default GeneralWeather
---
+++ components/SearchBar.tsx
@@ -0,0 +1,28 @@
+import {useState} from 'react'
+import { FiSearch } from "react-icons/fi";
+import { useRouter } from 'next/navigation';
+
+const SearchBar = () => {
+    const [searchTerm, setSearchTerm] = useState('');
+    const router = useRouter();
+
+    const handleSubmit = (e: { preventDefault: () => void; })=>{
+        e.preventDefault();
+        router.push(`/?search=${searchTerm}`);
+      }
+  return (
+    <form onSubmit={handleSubmit} autoComplete='off' className='p-2 text-gray-300 focus-within:text-gray-600'>
+        <label htmlFor="search-field" className='sr-only'>Search City Weather </label>
+        <div className='flex flex-row justify-start items-center'>
+            <FiSearch className="w-5 h-5 ml-4 text-black" />
+            <input type="search" autoComplete='off' id="search-field"
+            placeholder='Search City Weather'
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            className='flex-1 bg-transparent border-none outline-none placeholder-gray-500 text-base text-black p-4' />
+        </div>
+    </form>
+  )
+}
+
+export default SearchBar
