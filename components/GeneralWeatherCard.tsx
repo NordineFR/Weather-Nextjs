b33b4715ee26889c8dd0e0bcba4735f579462f51
NordineFR <farisseaz@outlook.fr>
2023-08-23 22:06:05 +0100

    added evey condition of weather

--- components/GeneralWeatherCard.tsx
+++ components/GeneralWeatherCard.tsx
@@ -48,60 +48,88 @@ interface Props<T> {
     fetchWeatherStyles();
   }, []);
 
-  const getWeatherStyles = async (weatherStatus: string,localHour:number): Promise<Styles> => {
+  const getWeatherStyles = async (weatherStatus: number,localHour:number): Promise<Styles> => {
     // Simulate fetching styles asynchronously (replace with actual API call)
     return new Promise<Styles>((resolve) => {
       setTimeout(() => {
         let backgroundColor = '';
         let color = '';
         let backgroundImage = '';
-
+        let isNight = false;
         if(localHour >= 21 || localHour < 6){
+          isNight = true;
           backgroundColor = '#0F1621';
           color = 'white';
           backgroundImage = "url('/images/clouds.jpg')";
-        }else{
-          switch (weatherStatus.toString()) {
-            case '1000': // Sunny
-              backgroundColor = '#c4e2ff';
-              color = '#24609b';
+        }
+          switch (weatherStatus) {
+            case 1000: // Sunny
+            case 1003: // Partly cloudy
+              backgroundColor = isNight ? '#0F1621' : '#c4e2ff';
+              color = isNight ? 'white' : '#24609b';
               backgroundImage = "url('/images/clouds.jpg')";
               break;
-            case '1003': // Partly cloudy
-              backgroundColor = '#c4e2ff';
-              color = '#24609b';
-              backgroundImage = "url('/images/clouds.jpg')";
+            case 1006: // Cloudy
+            case 1009: // Overcast
+            case 1030: // Mist
+            case 1063: // Patchy rain possible
+            case 1066: // Patchy snow possible
+            case 1069: // Patchy sleet possible
+            case 1072: // Patchy freezing drizzle possible
+            case 1087: // Thundery outbreaks possible
+            case 1114: // Blowing snow
+            case 1117: // Blizzard
+            case 1135: // Fog
+            case 1147: // Freezing fog
+            case 1150: // Patchy light drizzle
+            case 1153: // Light drizzle
+            case 1168: // Freezing drizzle
+            case 1171: // Heavy freezing drizzle
+              backgroundColor = isNight ? '#0F1621' : '#c4e2ff';
+              color = isNight ? 'white' : '#24609b';
               break;
-            case '1006': // Cloudy
-            case '1009': // Overcast
-              backgroundColor = '#0F1621';
-              color = 'white';
-              backgroundImage = "url('/images/clouds.jpg')";
+            case 1180: // Patchy light rain
+            case 1183: // Light rain
+            case 1186: // Moderate rain at times
+            case 1189: // Moderate rain
+            case 1192: // Heavy rain at times
+            case 1195: // Heavy rain
+              backgroundColor = isNight ? '#0F1621' : '#1e3046';
+              color = isNight ? 'white' : '#24609b';
               break;
-            case '1063': // Patchy rain possible
-            case '1066': // Patchy snow possible
-            case '1069': // Patchy sleet possible
-            case '1072': // Patchy freezing drizzle possible
-              backgroundColor = '#0F1621';
-              color = 'white';
+            case 1198: // Light freezing rain
+            case 1201: // Moderate or heavy freezing rain
+            case 1204: // Light sleet
+            case 1207: // Moderate or heavy sleet
+            case 1210: // Patchy light snow
+            case 1213: // Light snow
+            case 1216: // Patchy moderate snow
+            case 1219: // Moderate snow
+            case 1222: // Patchy heavy snow
+            case 1225: // Heavy snow
+            case 1237: // Ice pellets
+            case 1240: // Light rain shower
+            case 1243: // Moderate or heavy rain shower
+            case 1246: // Torrential rain shower
+            case 1249: // Light sleet showers
+            case 1252: // Moderate or heavy sleet showers
+            case 1255: // Light snow showers
+            case 1258: // Moderate or heavy snow showers
+            case 1261: // Light showers of ice pellets
+            case 1264: // Moderate or heavy showers of ice pellets
+            case 1273: // Patchy light rain with thunder
+            case 1276: // Moderate or heavy rain with thunder
+            case 1279: // Patchy light snow with thunder
+            case 1282: // Moderate or heavy snow with thunder
+              backgroundColor = isNight ? '#0F1621' : '#c4e2ff';
+              color = isNight ? 'white' : '#24609b';
               break;
-            case '1087': // Thundery outbreaks possible
-              backgroundColor = '#0F1621';
-              color = 'white';
-              break;
-            case '1114': // Blowing snow
-            case '1117': // Blizzard
-              backgroundColor = '#0F1621';
-              color = 'white';
-              break;
-            // Add more cases for other weather conditions and their corresponding colors
             default:
               backgroundColor = '#c4e2ff';
               color = '#24609b';
               backgroundImage = "url('/images/clouds.jpg')";
               break;
           }
-        }
 
         resolve({
           backgroundColor,
