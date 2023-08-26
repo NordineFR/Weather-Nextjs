4c191026f70d9825811ec1d6f13ab1321325b2a3
NordineFR <farisseaz@outlook.fr>
2023-08-26 12:14:03 +0100

    removed comment

--- components/GeneralWeatherCard.tsx
+++ components/GeneralWeatherCard.tsx
@@ -65,7 +65,6 @@ const GeneralWeather = ({ data }: Props<Record<string, any>>) => {
           color = "white";
           backgroundImage = "url('/images/clouds.jpg')";
         }
-        weatherStatus = 1000;
         switch (weatherStatus) {
           case 1000: // Sunny
             backgroundColor = isNight ? "#0F1621" : "#ffd89e";
