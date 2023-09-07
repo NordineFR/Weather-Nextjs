6b9e813cbe0ba244a1b103100a34ba68a6a0dc61
NordineFR <farisseaz@outlook.fr>
2023-09-07 21:46:44 +0100

    added dynamic marker with bounds

--- components/Map/Map.tsx
+++ components/Map/Map.tsx
@@ -1,44 +1,40 @@
 import ReactDOMServer from 'react-dom/server';
 import React, { useEffect, useState } from 'react';
-import { MapContainer, Marker, TileLayer, Popup } from 'react-leaflet';
+import { MapContainer, Marker, TileLayer, Popup ,useMap} from 'react-leaflet';
 import CustomMarkerIcon from '@/components/Map/CustomMarkerIcon'; 
 import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import Markers from './Markers';
 
 const WeatherMap = () => {
   const [weatherData, setWeatherData] = useState(null);
-
-  useEffect(() => {
-    const city = 'London'; // Replace with the desired city or location.
-
-    fetch(`https://api.weatherapi.com/v1/current.json?key=${process.env.NEXT_PUBLIC_WEATHER_API_KEY}&q=${city}`)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .then((data) => {
-        setWeatherData(data);
-      })
-      .catch((error) => {
-        console.error('Error fetching weather data:', error);
-      });
+  
+    useEffect(() => {
+      const city = 'London'; // Replace with the desired city or location.
+      fetch(`https://api.weatherapi.com/v1/current.json?key=${process.env.NEXT_PUBLIC_WEATHER_API_KEY}&q=${city}`)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.json();
+        })
+        .then((data) => {
+          setWeatherData(data);
+        })
+        .catch((error) => {
+          console.error('Error fetching weather data:', error);
+        });
     }, []);
-
-    function renderCustomIconToHTML(iconUrl, temperature) {
+    
+    function createCustomIcon(iconUrl: any, temperature: any) {
       const customIconContent = (
         <CustomMarkerIcon iconUrl={iconUrl} temperature={temperature} />
       );
-    
+
       const htmlString = ReactDOMServer.renderToStaticMarkup(customIconContent);
     
-      return htmlString;
-    }
-    function createCustomIcon(iconUrl, temperature) {
-      const customIconHTML = renderCustomIconToHTML(iconUrl, temperature);
-    
       return new L.DivIcon({
-        html: customIconHTML,
+        html: htmlString,
         className: 'custom-icon',
         iconSize: [40, 60], // Adjust the size as needed
       });
@@ -51,7 +47,7 @@ const WeatherMap = () => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        {weatherData && (
+        {/* {weatherData && (
           <Marker position={[weatherData?.location?.lat, weatherData?.location?.lon]} icon={createCustomIcon(weatherData?.current?.condition.icon,weatherData?.current?.temp_c)} >
             <Popup>
               <div>
@@ -62,6 +58,9 @@ const WeatherMap = () => {
               </div>
             </Popup>
           </Marker>
+        )} */}
+        {weatherData && (
+        <Markers weatherData={weatherData} />
         )}
       </MapContainer>
     </div>
