09e8bcc35dbb20e0ec0b68e5e88344c519b0f125
NordineFR <farisseaz@outlook.fr>
2023-09-09 21:50:57 +0100

    getting bbox based on map position

--- components/Map/Markers.tsx
+++ components/Map/Markers.tsx
@@ -14,29 +14,52 @@ const Markers = ({ weatherData }:Props<Record<string, any>>) => {
   const [visibleBounds, setVisibleBounds] = useState(null);
   const map = useMap();
 
-  // useEffect(() => {
-  //   // Update visible bounds when the map view changes
-  //   map.on('moveend', () => {
-  //     setVisibleBounds(map.getBounds());
-  //   });
-  // }, [map]);
+  useEffect(() => {
+    // Update visible bounds when the map view changes
+    map.on('moveend', () => {
+      setVisibleBounds(map.getBounds());
+    });
+  }, [map]);
+  
+  useEffect(() => {
+    if (!visibleBounds) return;
+    
+    // Calculate a bounding box around the visible bounds (you can adjust the buffer as needed)
+    const buffer = 0.2; // Adjust the buffer distance
+    const bounds = visibleBounds.pad(buffer);
+    const zoom = map.getZoom();
+    console.log(bounds);
+    // Fetch weather data for cities within the bounding box
+    fetchCitiesWeather(bounds,zoom).then((data: any) => {
+      setCloseWeatherData(data);
+    });
+    }, [visibleBounds]);
 
-  //  useEffect(() => {
-  //   if (!visibleBounds) return;
+  function fetchCitiesWeather (bounds: any, zoom: number){
 
-  //   // Calculate a bounding box around the visible bounds (you can adjust the buffer as needed)
-  //   const buffer = 0.2; // Adjust the buffer distance
-  //   const bounds = visibleBounds.pad(buffer);
-  //   const zoom = map.getZoom();
-  //   // Fetch weather data for cities within the bounding box
-  //   fetchCitiesWeather(bounds,zoom).then((data: any) => {
-  //     setCloseWeatherData(data);
-  //   });
-  //   }, [visibleBounds]);
+    const apiKey = '7f975775d821f079c63f64436355f924';
+    const bbox = `${bounds[0]},${bounds[1]},${bounds[2]},${bounds[3]}`;
+    // const apiUrl = `https://api.openweathermap.org/data/2.5/find?bbox=${bbox}&appid=${apiKey}`;
 
-  // function fetchCitiesWeather (bounds: any, zoom: number){
-  //   console.log(bounds);
-  // }
+    // const apiUrl = `https://api.example.com/weather?bounds=${bounds}&zoom=${zoom}`;
+
+    return new Promise((resolve, reject) => {
+      fetch(apiUrl)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.json();
+        })
+        .then((data) => {
+          resolve(data); // Resolve the Promise with the data
+        })
+        .catch((error) => {
+          console.error('Error fetching weather data:', error);
+          reject(error); // Reject the Promise with the error
+        });
+    });
+  }
 
   function createCustomIcon(iconUrl: any, temperature: any) {
     const customIconContent = (
