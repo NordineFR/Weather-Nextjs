73311bb5b853bfda52ea0b8dd8796f5e4a454209
NordineFR <farisseaz@outlook.fr>
2023-09-13 21:09:09 +0100

    added glass bg to custom marker

--- components/Map/CustomMarkerIcon.tsx
+++ components/Map/CustomMarkerIcon.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const CustomMarkerIcon = ({ iconUrl, temperature }) => {
   return (
-    <div className="custom-icon bg-opacity-80 bg-gray-300 rounded-md flex flex-col justify-center items-center w-11 h-fit border-[1px] border-gray-500">
-      <img src={`https://openweathermap.org/img/wn/${iconUrl}.png`} alt="Weather Icon"  className='h-10 w-12 object-contain'/>
-      <div className='font-bold text-[11px]'>{temperature}°C</div>
+    <div className="custom-icon flex flex-col justify-center items-center w-11 h-fit border-[1px] border-gray-400 bg-opacity-50 bg-blur-lg bg-gray-200 backdrop-blur-lg rounded-md">
+      <img src={`https://openweathermap.org/img/wn/${iconUrl}.png`} alt="Weather Icon"  className='h-11 w-12 object-contain '/>
+      <div className='font-bold text-[11px]'>{temperature} °C</div>
     </div>
   );
 };
