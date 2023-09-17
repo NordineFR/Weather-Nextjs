6298bff7b22015c68880dd85ff4b50c4a1e37198
NordineFR <farisseaz@outlook.fr>
2023-09-17 21:55:28 +0100

    fixing props type in cutommarker

--- components/Map/CustomMarkerIcon.tsx
+++ components/Map/CustomMarkerIcon.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
-const CustomMarkerIcon = ({ iconUrl, temperature }) => {
+interface Props {
+  iconUrl: string;
+  temperature: number;
+}
+const CustomMarkerIcon = ({ iconUrl, temperature }:Props) => {
   return (
     <div className="custom-icon flex flex-col justify-center items-center w-11 h-fit border-[1px] border-gray-400 bg-opacity-50 bg-blur-lg bg-gray-200 backdrop-blur-lg rounded-md">
       <img src={`https://openweathermap.org/img/wn/${iconUrl}.png`} alt="Weather Icon"  className='h-11 w-12 object-contain '/>
